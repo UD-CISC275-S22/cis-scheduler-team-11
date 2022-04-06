a84244bb82542d2f444de33bc47ef1d6bafe5438
andrewl15 <andrewl@udel.edu>
2022-04-06 14:04:50 -0400

    added interfaces

---
+++ src/interfaces/course.ts
@@ -0,0 +1,10 @@
+export interface Course {
+    /** A unique identifier for the course */
+    id: string;
+    /** The human-friendly title of the course */
+    title: string;
+    /** Number of credits for the course */
+    credits: string;
+    /** The current statis of the course */
+    statis: string;
+}
