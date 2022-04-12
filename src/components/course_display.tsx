7712da63d61eb4a0f8689c04158c27b80d8fd83d
suryanuj <suryanuj@udel.edu>
2022-04-11 20:42:24 -0400

    added all of the interfaces into one file.

--- src/components/course_display.tsx
+++ src/components/course_display.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { Course } from "../interfaces/course";
+import { Course } from "../interfaces/projectInterfaces";
 
 function course_display(course: Course): JSX.Element {
-    const { id, title, credits, status } = course;
+    const { id, title, credits, enrolled } = course;
     return (
         <div>
             {title} {id} <br />
             {credits} Credits
             <br />
-            Status: {status};<br />
+            Status: {enrolled};<br />
         </div>
     );
 }
---
+++ src/interfaces/projectInterfaces.ts
@@ -0,0 +1,29 @@
+export interface Course {
+    /** A unique identifier for the course */
+    id: string;
+    /** The human-friendly title of the course */
+    title: string;
+    /** Number of credits for the course */
+    credits: number;
+    /** The current status of the course */
+    enrolled: boolean;
+}
+
+export interface Semester {
+    /** The id of the Semester i.e. Fall 2022 */
+    id: string;
+    /** An array of the courses and their identifiers */
+    courses: Course[];
+}
+
+export interface MultSemester {
+    /** An array of the courses and their identifiers */
+    semesters: Semester[];
+}
+
+export interface Plan {
+    /** The id of the plan */
+    id: number;
+    /** An array of the Semesters and their courses */
+    semesters: Semester[];
+}
