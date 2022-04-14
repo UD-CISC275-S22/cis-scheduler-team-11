89838640c2ded4df43b2010f21c21730acba67e7
andrewl15 <andrewl@udel.edu>
2022-04-14 17:09:27 -0400

    working on bug

--- src/components/SemesterCreator.tsx
+++ src/components/SemesterCreator.tsx
@@ -7,20 +7,16 @@ import { Semester } from "../interfaces/projectInterfaces";
 //         ...course
 //     })
 // );
-const SEASONS = ["Fall", "Winter", "Spring", "Summer"];
-const DEFAULT_SEASON = SEASONS[0];
 
 export function SemesterCreator(): JSX.Element {
-    const [semesterSeason, setSemesterSeason] =
-        useState<string>(DEFAULT_SEASON);
+    const [semesterSeason, setSemesterSeason] = useState<string>("Fall");
     const [semesterYear, setSemesterYear] = useState<string>("2022");
     const [semesterList, setSemesterList] = useState<Semester[]>([]);
-
     function addSemester(season: string, year: string) {
         const semesterId = season + " " + year;
         const newSemester = { id: semesterId, courses: [] };
         const newSemesterList = [...semesterList, newSemester];
-        if (!semesterList.includes(newSemester)) {
+        if (semesterList.find(({ id }) => (id === semesterId) !== null)) {
             setSemesterList(newSemesterList);
         }
     }
