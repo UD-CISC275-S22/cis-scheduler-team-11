5082df541d6c0e65e1c3d3fee39830638e443b77
suryanuj <suryanuj@udel.edu>
2022-05-16 22:03:28 -0400

    added tests in a bunch of files

--- src/ComponentTests/MainView.test.tsx
+++ src/ComponentTests/MainView.test.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import { Plan } from "../interfaces/projectInterfaces";
 import { MainView } from "../components/MainView";
 
 describe("MainView tests", () => {
